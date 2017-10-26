ca85b33ad095e157769796dc5fc0b263f5c12dbf
nishlashakya <nishlashakya2@gmail.com>
2017-10-26 22:58:48 +0545

    adding authentication in react routes

--- src/routes.js
+++ src/routes.js
@@ -10,14 +10,20 @@ import ViewRecipePage from './modules/Recipe/viewRecipe';
 import ViewRecipeDetail from './modules/Recipe/viewRecipeDetail';
 import EditRecipePage from './modules/Recipe/editRecipe';
 
+function requireAuth(nextState, replace, callback) {
+	const login = localStorage.getItem('user');
+	if (!login) replace ('/login')
+	return callback()
+}
+
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={ViewRecipePage} />
 		<Route path="/login" component={LoginPage} />
 		<Route path="/register" component={RegisterPage} />
-		<Route path="/recipe/add" component={AddRecipePage} />
+		<Route path="/recipe/add" component={AddRecipePage} onEnter={requireAuth}/>
 		<Route path="/recipe/view" component={ViewRecipePage} />
 		<Route path="/recipe/view/:id" component={ViewRecipeDetail} />
-		<Route path="/recipe/edit/:id" component={EditRecipePage} />
+		<Route path="/recipe/edit/:id" component={EditRecipePage} onEnter={requireAuth}/>
 	</Route>
 )
